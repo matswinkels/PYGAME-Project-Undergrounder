6b3e43c329ed9f60e32ee13aa11d247bedbe880a
matswinkels <m_sliwakowski@wp.pl>
2021-04-13 15:54:57 +0200

    rendering changes

--- textures.py
+++ textures.py
@@ -1,10 +1,14 @@
 import pygame
 
 TEXTURES = {                                                    # ID
-    'stone':        pygame.image.load('img/stone2.png'),        # 4
+    'character_front':      pygame.image.load('img/character.png'),
+    'character_left1':      pygame.image.load('img/character2.png'),
+    'character_right1':     pygame.image.load('img/character3.png'),
     'dirt':         pygame.image.load('img/dirt2.png'),         # 1
     'water':        pygame.image.load('img/water2.png'),        # 2
     'barrier':      pygame.image.load('img/barrier2.png'),      # 3
-    'character':    pygame.image.load('img/character.png'),     
-    'berry':        pygame.image.load('img/berry.png')          # 5
+    'stone':        pygame.image.load('img/stone2.png'),        # 4
+    'berry':        pygame.image.load('img/berry.png'),         # 5
+    'bush':         pygame.image.load('img/bush.png'),          # 6
+    'deer':         pygame.image.load('img/deer.png')
 }
